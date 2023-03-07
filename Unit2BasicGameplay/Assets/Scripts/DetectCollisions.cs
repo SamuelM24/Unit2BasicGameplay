0846157a99ba499c93cb40309cd22475da663128
SamuelM24 <beniciomedina11@gmail.com>
2023-03-07 09:57:23 -0800

    finished part 3

--- Unit2BasicGameplay/Assets/Scripts/DetectCollisions.cs
+++ Unit2BasicGameplay/Assets/Scripts/DetectCollisions.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnManager : MonoBehaviour
+public class DetectCollisions : MonoBehaviour
 {
-    public GameObject[] animalPrefabs;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +13,6 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-
+        
     }
 }
