e28dd309c71a47da014166199be9953ba81f5899
SamuelM24 <beniciomedina11@gmail.com>
2023-03-09 09:57:49 -0800

    finished

--- Unit2BasicGameplay/Assets/Scripts/DetectCollisions.cs
+++ Unit2BasicGameplay/Assets/Scripts/DetectCollisions.cs
@@ -7,12 +7,18 @@ public class DetectCollisions : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        Destroy(other.gameObject);
     }
 }
